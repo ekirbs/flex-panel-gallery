6cdf4fad1b20532ec0a5d0cfc08bd9b287b670b9
Eric K <erickirberger@gmail.com>
2023-11-27 16:13:11 -0500

    little changes

--- indexSandbox.js
+++ indexSandbox.js
@@ -1,31 +1,63 @@
+// const panels = document.querySelectorAll('.panel');
+// let timeout;
+
+// // function toggleOpenWithDelay() {
+// //   clearTimeout(timeout);
+// //   const panel = this;
+// //   timeout = setTimeout(() => {
+// //     panel.classList.add('open');
+// //   }, 500); // Adjust this delay as needed
+// // };
+
+// // function toggleOpen() {
+// //   clearTimeout(timeout);
+// //   this.classList.add('open');
+// // };
+
+// // function toggleClose() {
+// //   this.classList.remove('open');
+// // };
+
+// function toggleActive(e) {
+//   if(e.propertyName.includes('flex')) {
+//     this.classList.toggle('open-active');
+//   }
+// };
+
+// panels.forEach(panel => {
+//   // panel.addEventListener('mouseenter', toggleOpenWithDelay);
+//   // panel.addEventListener('mouseleave', toggleClose);
+//   panel.addEventListener('transitionend', toggleActive);
+// });
+
 const panels = document.querySelectorAll('.panel');
-let timeout;
+let isTransitioning = false;
 
-// function toggleOpenWithDelay() {
-//   clearTimeout(timeout);
-//   const panel = this;
-//   timeout = setTimeout(() => {
-//     panel.classList.add('open');
-//   }, 500); // Adjust this delay as needed
-// };
-
-// function toggleOpen() {
-//   clearTimeout(timeout);
-//   this.classList.add('open');
-// };
-
-// function toggleClose() {
-//   this.classList.remove('open');
-// };
+function toggleOpen() {
+  if (!isTransitioning) {
+    isTransitioning = true;
+    this.classList.add('open');
+    setTimeout(() => {
+      isTransitioning = false;
+    }, 700); // Adjust this duration to match your transition time
+  }
+}
 
 function toggleActive(e) {
-  if(e.propertyName.includes('flex')) {
+  if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
   }
-};
+}
 
 panels.forEach(panel => {
-  // panel.addEventListener('mouseenter', toggleOpenWithDelay);
-  // panel.addEventListener('mouseleave', toggleClose);
+  panel.addEventListener('mouseenter', toggleOpen);
   panel.addEventListener('transitionend', toggleActive);
+});
+
+panels.forEach(panel => {
+  panel.addEventListener('mouseleave', function() {
+    if (!isTransitioning) {
+      this.classList.remove('open');
+    }
+  });
 });
